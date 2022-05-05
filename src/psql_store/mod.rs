f4e9e66c869c505ae213b272f24e42898c1e23e6
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-05 17:00:30 +0100

    Add the remainder of save_changes

--- src/psql_store/mod.rs
+++ src/psql_store/mod.rs
@@ -10,13 +10,14 @@ use matrix_sdk::ruma::events::presence::PresenceEvent;
 use matrix_sdk::ruma::events::receipt::Receipt;
 use matrix_sdk::ruma::events::room::member::{MembershipState, RoomMemberEventContent};
 use matrix_sdk::ruma::events::{
-    AnyGlobalAccountDataEvent, AnyRoomAccountDataEvent, AnySyncStateEvent,
-    GlobalAccountDataEventType, RoomAccountDataEventType, StateEventType,
+    AnyGlobalAccountDataEvent, AnyRoomAccountDataEvent, AnyStrippedStateEvent, AnySyncStateEvent,
+    GlobalAccountDataEventType, OriginalSyncStateEvent, RoomAccountDataEventType, StateEventType,
+    StrippedStateEvent,
 };
 use matrix_sdk::ruma::receipt::ReceiptType;
 use matrix_sdk::ruma::serde::Raw;
 use matrix_sdk::ruma::{EventId, MxcUri, OwnedEventId, OwnedUserId, RoomId, UserId};
-use matrix_sdk::{async_trait, RoomInfo, StateChanges};
+use matrix_sdk::{async_trait, RoomInfo, StateChanges, StoreError};
 use matrix_sdk_base::store::{BoxStream, Result as StateResult};
 use matrix_sdk_base::StateStore;
 use serde::Serialize;
@@ -152,7 +153,7 @@ impl PostgresStateStore {
         txn: &mut Transaction<'_, Postgres>,
         room: impl AsRef<str> + Send,
         member: impl AsRef<str> + Send,
-        event: &(impl Serialize + Send + Sync),
+        event: &OriginalSyncStateEvent<RoomMemberEventContent>,
     ) -> Result<()> {
         query!(
             r#"
@@ -166,6 +167,295 @@ impl PostgresStateStore {
             room.as_ref(),
             member.as_ref(),
             Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates a user profile
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_profile(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        user_id: impl AsRef<str> + Send,
+        profile: &RoomMemberEventContent,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_profiles
+                    (room_id, user_id, profile_data)
+                VALUES
+                    ($1, $2, $3)
+                ON CONFLICT (room_id, user_id)
+                    DO UPDATE SET profile_data = EXCLUDED.profile_data
+            "#,
+            room.as_ref(),
+            user_id.as_ref(),
+            Json(profile) as _
+        )
+        .execute(txn)
+        .await?;
+
+        Ok(())
+    }
+
+    /// Updates a display name
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_displayname(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        user_id: impl AsRef<str> + Send,
+        displayname: impl AsRef<str> + Send,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_displaynames
+                    (room_id, user_id, displayname)
+                VALUES
+                    ($1, $2, $3)
+                ON CONFLICT (room_id, user_id)
+                    DO UPDATE SET displayname = EXCLUDED.displayname
+            "#,
+            room.as_ref(),
+            user_id.as_ref(),
+            displayname.as_ref()
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates a your account data
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_account_data(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        event_type: impl AsRef<str> + Send,
+        event: &Raw<AnyGlobalAccountDataEvent>,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_accountdata
+                    (event_type, event_data)
+                VALUES
+                    ($1, $2)
+                ON CONFLICT (event_type)
+                    DO UPDATE SET event_data = EXCLUDED.event_data
+            "#,
+            event_type.as_ref(),
+            Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates a your account data for a specific room
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_room_account_data(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        to_string: impl AsRef<str> + Send,
+        event: &Raw<AnyRoomAccountDataEvent>,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_room_account_data
+                    (room_id, event_type, account_data)
+                VALUES
+                    ($1, $2, $3)
+                ON CONFLICT (room_id, event_type)
+                    DO UPDATE SET account_data = EXCLUDED.account_data
+            "#,
+            room.as_ref(),
+            to_string.as_ref(),
+            Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates room info
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_room_info(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room_id: impl AsRef<str> + Send,
+        room_info: &RoomInfo,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_stripped_room_infos
+                    (room_id, room_info)
+                VALUES
+                    ($1, $2)
+                ON CONFLICT (room_id)
+                    DO UPDATE SET room_info = EXCLUDED.room_info
+            "#,
+            room_id.as_ref(),
+            Json(room_info) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates User presence
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_presence(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        user_id: impl AsRef<str> + Send,
+        event: &Raw<PresenceEvent>,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_presence
+                    (user_id, presence_event)
+                VALUES
+                    ($1, $2)
+                ON CONFLICT (user_id)
+                    DO UPDATE SET presence_event = EXCLUDED.presence_event
+            "#,
+            user_id.as_ref(),
+            Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates Stripped room info
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn update_stripped_room_info(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room_id: impl AsRef<str> + Send,
+        info: &RoomInfo,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_stripped_room_infos
+                    (room_id, room_info)
+                VALUES
+                    ($1, $2)
+                ON CONFLICT (room_id)
+                    DO UPDATE SET room_info = EXCLUDED.room_info
+            "#,
+            room_id.as_ref(),
+            Json(info) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates Stripped member info
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn save_stripped_member(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        state_key: impl AsRef<str> + Send,
+        event: &StrippedStateEvent<RoomMemberEventContent>,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_stripped_members
+                    (room_id, state_key, member_event)
+                VALUES
+                    ($1, $2, $3)
+                ON CONFLICT (room_id, state_key)
+                    DO UPDATE SET member_event = EXCLUDED.member_event
+            "#,
+            room.as_ref(),
+            state_key.as_ref(),
+            Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates Stripped event state
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn save_stripped_state(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        event_type: impl AsRef<str> + Send,
+        state_key: impl AsRef<str> + Send,
+        event: &Raw<AnyStrippedStateEvent>,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_stripped_room_state
+                    (room_id, event_type, state_key, state_event)
+                VALUES
+                    ($1, $2, $3, $4)
+                ON CONFLICT (room_id, event_type, state_key)
+                    DO UPDATE SET state_event = EXCLUDED.state_event
+            "#,
+            room.as_ref(),
+            event_type.as_ref(),
+            state_key.as_ref(),
+            Json(event) as _
+        )
+        .execute(txn)
+        .await?;
+        Ok(())
+    }
+
+    /// Updates Room receipt state
+    ///
+    /// # Errors
+    /// This function will return an error if updating the database fails
+    async fn save_room_receipts(
+        &self,
+        txn: &mut Transaction<'_, Postgres>,
+        room: impl AsRef<str> + Send,
+        event_id: impl AsRef<str> + Send,
+        receipt_type: impl AsRef<str> + Send,
+        user_id: impl AsRef<str> + Send,
+        receipt: &Receipt,
+    ) -> Result<()> {
+        query!(
+            r#"
+                INSERT INTO statestore_room_receipts
+                    (room_id, receipt_type, user_id, event_id, receipt)
+                VALUES
+                    ($1, $2, $3, $4, $5)
+                ON CONFLICT (room_id, receipt_type, user_id)
+                    DO UPDATE SET event_id = EXCLUDED.event_id, receipt = EXCLUDED.receipt
+            "#,
+            room.as_ref(),
+            receipt_type.as_ref(),
+            user_id.as_ref(),
+            event_id.as_ref(),
+            Json(receipt) as _
         )
         .execute(txn)
         .await?;
@@ -199,6 +489,89 @@ impl PostgresStateStore {
                             .await?;
                     }
                 }
+                self.save_member(&mut txn, room, &event.state_key, event)
+                    .await?;
+            }
+        }
+
+        for (room, users) in &changes.profiles {
+            for (user_id, profile) in users {
+                self.update_profile(&mut txn, room, user_id, profile)
+                    .await?;
+            }
+        }
+
+        for (room, map) in &changes.ambiguity_maps {
+            for (display_name, user_ids) in map {
+                for user_id in user_ids {
+                    self.update_displayname(&mut txn, room, user_id, display_name)
+                        .await?;
+                }
+            }
+        }
+
+        for (event_type, event) in &changes.account_data {
+            self.update_account_data(&mut txn, event_type.to_string(), event)
+                .await?;
+        }
+
+        for (room, events) in &changes.room_account_data {
+            for (event_type, event) in events {
+                self.update_room_account_data(&mut txn, room, event_type.to_string(), event)
+                    .await?;
+            }
+        }
+
+        for (room_id, room_info) in &changes.room_infos {
+            self.update_room_info(&mut txn, room_id, room_info).await?;
+        }
+
+        for (sender, event) in &changes.presence {
+            self.update_presence(&mut txn, sender, event).await?;
+        }
+
+        for (room_id, info) in &changes.stripped_room_infos {
+            self.update_stripped_room_info(&mut txn, room_id, info)
+                .await?;
+        }
+
+        for (room, events) in &changes.stripped_members {
+            for event in events.values() {
+                self.save_stripped_member(&mut txn, room, &event.state_key, event)
+                    .await?;
+            }
+        }
+
+        for (room, event_types) in &changes.stripped_state {
+            for (event_type, events) in event_types {
+                for (state_key, event) in events {
+                    self.save_stripped_state(
+                        &mut txn,
+                        room,
+                        event_type.to_string(),
+                        state_key,
+                        event,
+                    )
+                    .await?;
+                }
+            }
+        }
+
+        for (room, content) in &changes.receipts {
+            for (event_id, receipts) in &content.0 {
+                for (receipt_type, receipts) in receipts {
+                    for (user_id, receipt) in receipts {
+                        self.save_room_receipts(
+                            &mut txn,
+                            room,
+                            event_id,
+                            receipt_type,
+                            user_id,
+                            receipt,
+                        )
+                        .await?;
+                    }
+                }
             }
         }
 
@@ -224,12 +597,18 @@ impl StateStore for PostgresStateStore {
     /// * `filter_id` - The filter id that should be stored in the state store.
     #[allow(clippy::panic)]
     async fn save_filter(&self, filter_name: &str, filter_id: &str) -> StateResult<()> {
-        Ok(self.save_filter(filter_name, filter_id).await?)
+        Ok(self
+            .save_filter(filter_name, filter_id)
+            .await
+            .map_err(|e| StoreError::Backend(e.into()))?)
     }
 
     /// Save the set of state changes in the store.
     async fn save_changes(&self, changes: &StateChanges) -> StateResult<()> {
-        Ok(self.save_changes(changes).await?)
+        Ok(self
+            .save_changes(changes)
+            .await
+            .map_err(|e| StoreError::Backend(e.into()))?)
     }
 
     /// Get the filter id that was stored under the given filter name.
@@ -496,21 +875,6 @@ impl StateStore for PostgresStateStore {
     ///
     /// * `room_id` - The `RoomId` of the room to delete.
     async fn remove_room(&self, room_id: &RoomId) -> StateResult<()> {
-        todo!();
-    }
-
-    /// Get a stream of the stored timeline
-    ///
-    /// # Arguments
-    ///
-    /// * `room_id` - The `RoomId` of the room to delete.
-    ///
-    /// Returns a stream of events and a token that can be used to request
-    /// previous events.
-    async fn room_timeline(
-        &self,
-        room_id: &RoomId,
-    ) -> StateResult<Option<(BoxStream<StateResult<SyncRoomEvent>>, Option<String>)>> {
         todo!();
     }
 }
