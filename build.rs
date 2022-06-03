d84ed34621f84bfcf8d8975952f9142675807f65
Charlotte <lotte@chir.rs>
2022-06-03 20:11:07 +0100

    Revert "don’t gather git version info in nix"
    This reverts commit d79d7f33291eb35e4a5a0f28c1eff9198812ea28.

--- build.rs
+++ build.rs
@@ -1,10 +1,3 @@
-use std::env;
-
 fn main() -> anyhow::Result<()> {
-    let mut config = vergen::Config::default();
-    if env::var("NIX_CC").is_ok() {
-        // Don’t use git information when building with nix-build
-        *config.git_mut().enabled_mut() = false;
-    }
     vergen::vergen(vergen::Config::default())
 }
