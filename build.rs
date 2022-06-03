1de0e106daa758cddca80960dbfaf8b45b792049
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-03 20:18:12 +0100

    remove version calculation for now

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 use std::env;
 
 fn main() -> anyhow::Result<()> {
-    let mut config = vergen::Config::default();
+    /*let mut config = vergen::Config::default();
     if env::var("NIX_CC").is_ok() {
         // Don’t use git information when building with nix-build
         *config.git_mut().enabled_mut() = false;
     }
-    vergen::vergen(vergen::Config::default())
+    vergen::vergen(vergen::Config::default())*/
+    Ok(())
 }
