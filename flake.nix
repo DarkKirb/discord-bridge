c794675c50949556c27e6bc18c1ceaa73afdbbb4
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-19 07:37:41 +0100

    feat: Expose cargo deps as legacyPackages

--- flake.nix
+++ flake.nix
@@ -19,37 +19,66 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix, ... }@inputs:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      let
-        overlays = [ cargo2nix.overlays.default (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          packageFun = import ./Cargo.nix;
-          rustChannel = "1.60.0";
-          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    cargo2nix,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [cargo2nix.overlays.default (import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        packageFun = import ./Cargo.nix;
+        rustChannel = "1.60.0";
+        packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+      };
+    in rec {
+      devShells.default = with pkgs;
+        mkShell {
+          buildInputs = [
+            (rust-bin.nightly.latest.default.override {
+              extensions = ["rust-src"];
+            })
+            cargo2nix.packages.${system}.cargo2nix
+            cargo-audit
+            sqlx-cli
+            github-cli
+            mold
+            clang
+            statix
+          ];
         };
-      in rec {
-        devShells.default = with pkgs;
-          mkShell {
-            buildInputs = [
-              (rust-bin.nightly.latest.default.override {
-                extensions = [ "rust-src" ];
-              })
-              cargo2nix.packages.${system}.cargo2nix
-              cargo-audit
-              sqlx-cli
-              github-cli
-              mold
-              clang
-            ];
-          };
-        packages = rec {
-          discord-matrix-bridge = rustPkgs.workspace.discord-matrix-bridge { };
-          default = discord-matrix-bridge;
+      legacyPackages = let
+        inherit (builtins) mapAttrs replaceStrings;
+        inherit (pkgs.lib.attrsets) mapAttrs';
+        pkg_unfixed = {
+          inherit (rustPkgs) unknown;
+          crates-io = rustPkgs."registry+https://github.com/rust-lang/crates.io-index";
+          matrix-rust-sdk = rustPkgs."git+https://github.com/matrix-org/matrix-rust-sdk";
+          matrix-sdk-sql = rustPkgs."git+https://github.com/DarkKirb/matrix-sdk-statestore-sql";
+          twilight = rustPkgs."git+https://github.com/terminal-discord/twilight";
+          vodozemac = rustPkgs."git+https://github.com/matrix-org/vodozemac";
         };
-        nixosModules.default = import ./nixos { inherit inputs system; };
-        hydraJobs = packages;
-        formatter = pkgs.nixfmt;
-      });
+      in
+        mapAttrs (_:
+          mapAttrs (_:
+            mapAttrs' (key: value: {
+              name = replaceStrings ["." "+"] ["-" "-"] key;
+              value = value {};
+            })))
+        pkg_unfixed;
+      packages = rec {
+        discord-matrix-bridge = rustPkgs.workspace.discord-matrix-bridge {};
+        default = discord-matrix-bridge;
+      };
+      nixosModules.default = import ./nixos {inherit inputs system;};
+      hydraJobs =
+        if pkgs.lib.strings.hasSuffix "-linux" system
+        then legacyPackages
+        else {};
+      formatter = pkgs.alejandra;
+    });
 }
